87e2de9954b73a0bbcdfce30f1361fb1380d1ec0
Luis Romero <lromero@alcancia.biz>
2024-02-07 13:31:00 -0400

    Added carrier privileges message

--- app/src/main/java/com/octolytics/octopulse/octopulsecarriertest/MainActivity.kt
+++ app/src/main/java/com/octolytics/octopulse/octopulsecarriertest/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.octolytics.octopulse.octopulsecarriertest
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.TelephonyManager
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val telephonyManager = (getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager)
-        Toast.makeText(this, telephonyManager?.hasCarrierPrivileges().toString(), Toast.LENGTH_LONG)
+        checkCarrierPrivileges()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkCarrierPrivileges()
+    }
+
+    private fun checkCarrierPrivileges() {
+        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager
+        val textView = findViewById<TextView>(R.id.tv_privileges)
+
+        if (telephonyManager?.hasCarrierPrivileges() == true) {
+            textView.setTextColor(Color.GREEN)
+            textView.text = "Tiene privilegios de operador"
+            textView.visibility = View.VISIBLE
+        } else {
+            textView.setTextColor(Color.RED)
+            textView.text = "No tiene privilegios de operador"
+            textView.visibility = View.VISIBLE
+        }
     }
 }
