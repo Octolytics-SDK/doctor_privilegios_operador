1cf8164aec15adcce1930447bc77cad8ae382908
lromeroalc <lromero@alcancia.biz>
2024-02-13 10:50:16 -0400

    adding refresh button

--- app/src/main/java/com/octolytics/octopulse/octopulsecarriertest/MainActivity.kt
+++ app/src/main/java/com/octolytics/octopulse/octopulsecarriertest/MainActivity.kt
@@ -6,9 +6,16 @@ import android.os.Bundle
 import android.telephony.TelephonyManager
 import android.text.TextPaint
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager
         val textView = findViewById<TextView>(R.id.tv_privileges)
         val iconView = findViewById<ImageView>(R.id.icon_privileges)
+        val buttonView = findViewById<Button>(R.id.btn_refresh)
+
+        buttonView.setOnClickListener {
+            refresh()
+        }
 
         if (telephonyManager?.hasCarrierPrivileges() == true) {
             textView.setTextColor(Color.GREEN)
@@ -42,6 +54,21 @@ class MainActivity : AppCompatActivity() {
         textView.visibility = View.VISIBLE
         iconView.visibility = View.VISIBLE
         addTextBorder(textView)
+    }
+
+    private fun refresh() {
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val iconView = findViewById<ImageView>(R.id.icon_privileges)
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            runCatching {
+                iconView.isVisible = false
+                progressBar.isVisible = true
+                delay(1500)
+                progressBar.isVisible = false
+                checkCarrierPrivileges()
+            }
+        }
     }
 
     private fun addTextBorder(textView: TextView) {
